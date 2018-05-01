01a1f0e4582e749ee281085cc573920fda39a6b2
Douile <tom.k.hipwell@gmail.com>
2018-05-01 21:55:54 +0100

    Stats

--- modules/data/stats.py
+++ modules/data/stats.py
@@ -2,15 +2,16 @@ from . import trackernetwork
 
 def getStats(key,name,platform):
     response = trackernetwork.StatsRequest(key,name,platform).send()
-    stats = {}
     if response.data != None:
+        stats = {}
         stats['kdr'] = response.data.lifetimestats.stat('kdr')
         stats['kills'] = response.data.lifetimestats.stat('kills')
         stats['wins'] = response.data.lifetimestats.stat('wins')
         stats['win%'] = response.data.lifetimestats.stat('winpercent')
         stats['games'] = response.data.lifetimestats.stat("matchesplayed")
+        data = {'name':response.data.epicUserHandle, 'platform': response.data.platformNameLong, 'stats':stats}
     else:
         print(response.status)
         print(response.headers)
-    data = {'name':response.data.epicUserHandle, 'platform': response.data.platformNameLong, 'stats':stats}
+    data = {'name':response.status,'platform':''}
     return data
