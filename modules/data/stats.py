4005bd1016be8a4aa884bb684bdedebc4ec7e174
Douile <tom.k.hipwell@gmail.com>
2018-05-01 21:27:57 +0100

    Testing

--- modules/data/stats.py
+++ modules/data/stats.py
@@ -3,10 +3,14 @@ from . import trackernetwork
 def getStats(key,name,platform):
     response = trackernetwork.StatsRequest(key,name,platform).send()
     stats = {}
-    stats['kdr'] = response.data.lifetimestats.stat('kdr')
-    stats['kills'] = response.data.lifetimestats.stat('kills')
-    stats['wins'] = response.data.lifetimestats.stat('wins')
-    stats['win%'] = response.data.lifetimestats.stat('winpercent')
-    stats['games'] = response.data.lifetimestats.stat("matchesplayed")
+    if response.data.lifetimestats != None:
+        stats['kdr'] = response.data.lifetimestats.stat('kdr')
+        stats['kills'] = response.data.lifetimestats.stat('kills')
+        stats['wins'] = response.data.lifetimestats.stat('wins')
+        stats['win%'] = response.data.lifetimestats.stat('winpercent')
+        stats['games'] = response.data.lifetimestats.stat("matchesplayed")
+    else:
+        print(response.status)
+        print(response.headers)
     data = {'name':response.data.epicUserHandle, 'platform': response.data.platformNameLong, 'stats':stats}
     return data
