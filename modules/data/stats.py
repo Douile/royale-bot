ed7b2ce88793b6bea940beccb489ef27f146d5ca
Douile <tom.k.hipwell@gmail.com>
2018-05-01 21:47:33 +0100

    Bug

--- modules/data/stats.py
+++ modules/data/stats.py
@@ -3,7 +3,7 @@ from . import trackernetwork
 def getStats(key,name,platform):
     response = trackernetwork.StatsRequest(key,name,platform).send()
     stats = {}
-    if response.data.lifetimestats != None:
+    if response.data != None:
         stats['kdr'] = response.data.lifetimestats.stat('kdr')
         stats['kills'] = response.data.lifetimestats.stat('kills')
         stats['wins'] = response.data.lifetimestats.stat('wins')
