47f64e7ea14de93600bcb40208bd5c86d5d65fb9
Douile <tom.k.hipwell@gmail.com>
2018-05-14 15:12:44 +0100

    Logging

--- dataretrieval/aiofnotes.py
+++ dataretrieval/aiofnotes.py
@@ -102,6 +102,8 @@ def parse_detail_patchnotes(html):
 @asyncio.coroutine
 def parse_simple_patchnotes(html):
     contents = {'description':'','extra':[],'video':None}
+    if len(str(contents)) < 300:
+        print(contents)
     outer = html.find('div', attrs={'class': 'patch-notes-description'})
     inner = outer.findChild('div')
     content = inner.findAll('p',recursive=False)
