f2edb2e706fe958c0eb28e02b4d4f621662a104b
Douile <tom.k.hipwell@gmail.com>
2018-05-16 18:44:59 +0100

    Shard test

---
+++ shard_test.py
@@ -0,0 +1,17 @@
+import os
+import threading
+
+THREADS = 2
+
+class BotThread:
+    def __init__(self, shard_id, shard_count):
+        self.shard_id = shard_id
+        self.shard_count = shard_count
+    def run(self):
+        os.system('python bot.py {} {}'.format(self.shard_id,self.shard_count))
+
+
+if __name__ == '__main__':
+    for i in range(THREADS):
+        thread = BotThread(i,THREADS)
+        threading.Thread(target=thread.run).start()
