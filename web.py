b6c6dd162bd2bec3edd98740c839425f4067b62b
Douile <tom.k.hipwell@gmail.com>
2018-05-18 15:27:07 +0100

    error

--- web.py
+++ web.py
@@ -21,7 +21,7 @@ class S(BaseHTTPRequestHandler):
         raw_data = self.rfile.read()
         data = get.handle(self.path,self.headers,raw_data)
         self._set_headers(data.code,data.type)
-        self.wfile.write(data.response)
+        self.wfile.write(str(data.response, 'utf-8'))
 
     def do_HEAD(self):
         self._set_headers(200,'text/json')
@@ -30,7 +30,7 @@ class S(BaseHTTPRequestHandler):
         raw_data = self.rfile.read()
         data = post.handle(self.path,self.headers,raw_data)
         self._set_headers(data.code,data.type)
-        self.wfile.write(data.response)
+        self.wfile.write(str(data.response, 'utf-8'))
 
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
