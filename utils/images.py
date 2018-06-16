d08948a1982816295c3e2ec325c1c7a93a783dab
Douile <tom.k.hipwell@gmail.com>
2018-06-16 23:31:30 +0100

    Ironing

--- utils/images.py
+++ utils/images.py
@@ -77,6 +77,7 @@ class Background:
     @staticmethod
     @asyncio.coroutine
     def reCropImage(image,size):
+        logger = logging.getLogger('bg-resize')
         # if image.height / size[1] > image.width / size[0]:
         #     width = size[0]
         #     height = round(size[1] / image.width * image.height)
@@ -101,14 +102,16 @@ class Background:
         else:
             nw = round(image.width / rh)
             nh = size[1]
+        logger.debug('Resized background o:(%d,%d) r:(%d,%d) n:(%d,%d)',image.width,image.height,rw,rh,nw,nh)
         image = image.resize((nw,nh))
-        logging.getLogger('bg-resize').debug('Resized background o:(%d,%d) r:(%d,%d) n:(%d,%d)',image.width,image.height,rw,rh,nw,nh)
         if image.width > size[0]:
             left = round((image.width - size[0]) / 2)
             right = left + size[0]
+            logger.debug('Cropped horizontal ow: %d left: %d right: %d',image.width,left,right)
             image.crop((left,0,right,image.height))
         if image.height > size[1]:
             top = round((image.height - size[1]) / 2)
             bottom = top + size[1]
+            logger.debug('Cropped vertical oh: %d top: %d bottom: %d',image.height,top,bottom)
             image.crop((0,top,image.width,bottom))
         return image
