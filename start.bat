f222c7f7163fca764fd673e9983ca847bcf448f2
Tom <25043847+Douile@users.noreply.github.com>
2018-12-26 19:52:34 +0000

    Fixes

--- start.bat
+++ start.bat
@@ -7,13 +7,13 @@ echo Setting env vars
 rem enter you own variables here
 
 rem discord bot key
-SET KEY_DISCORD="YOUR_KEY"
+SET KEY_DISCORD=YOUR_KEY
 rem fnbr.co api key
-SET KEY_FNBR="YOUR_KEY"
+SET KEY_FNBR=YOUR_KEY
 rem fortnite tracker network api key
-SET KEY_TRACKERNETWORK="YOUR_KEY"
+SET KEY_TRACKERNETWORK=YOUR_KEY
 rem your postgresql access url (see README for details on setting up)
-SET DATABASE_URL="postgres://my.database"
+SET DATABASE_URL=postgres://my.database
 
 echo Starting...
 python ./bot.py
